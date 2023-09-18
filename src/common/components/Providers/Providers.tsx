72afcb6fdce80ef7defd75a8edc946ef906b250d
odinokun <odinokun@gmail.com>
2023-09-18 09:32:28 +0200

    feat: Providers component created

---
+++ src/common/components/Providers/Providers.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../../../theme/theme';
+
+interface IProps {
+  children: ReactNode;
+}
+
+export const Providers: FC<IProps> = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
