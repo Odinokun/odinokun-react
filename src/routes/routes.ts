8276e9399cab1ceca5118bf3bf53fa37991c2c66
odinokun <odinokun@gmail.com>
2023-09-18 16:03:26 +0200

    feat: routes created

---
+++ src/routes/routes.ts
@@ -0,0 +1,34 @@
+import { lazy } from 'react';
+
+export enum ROUTES {
+  HOME = '/',
+  CV = 'cv',
+  WORKS = 'works',
+  BLOG = 'team',
+  CONTACTS = 'contacts',
+  NOT_FOUND = '*',
+}
+
+export const HomePage = lazy(
+  () => import('../pages/Home' /* webpackChunkName: "HomePage" */)
+);
+
+export const CvPage = lazy(
+  () => import('../pages/Cv' /* webpackChunkName: "CvPage" */)
+);
+
+export const WorksPage = lazy(
+  () => import('../pages/Works' /* webpackChunkName: "WorksPage" */)
+);
+
+export const BlogPage = lazy(
+  () => import('../pages/Blog' /* webpackChunkName: "BlogPage" */)
+);
+
+export const ContactsPage = lazy(
+  () => import('../pages/Contacts' /* webpackChunkName: "ContactsPage" */)
+);
+
+export const NotFoundPage = lazy(
+  () => import('../pages/NotFound' /* webpackChunkName: "NotFoundPage" */)
+);
