64129ba31f8552433c537678419067a03f5d90fa
odinokun <odinokun@gmail.com>
2023-09-18 09:37:40 +0200

    feat: absolutly pathes added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
+// don't forget update tsconfig.json for path aliases
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@common': '/src/common',
+      '@modules': '/src/modules',
+      '@pages': '/src/pages',
+      '@routes': '/src/routes',
+      '@theme': '/src/theme',
+      '@typings': '/src/typings',
+    },
+  },
+});
