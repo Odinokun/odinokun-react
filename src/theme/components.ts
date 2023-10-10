916ba3bc2dc57c555be26f41504b61ce472a459a
odinokun <odinokun@gmail.com>
2023-10-10 21:54:26 +0200

    feat: default styles for Container component added

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -2,15 +2,15 @@ export const components = {
   MuiContainer: {
     styleOverrides: {
       root: {
-        maxWidth: '1400px',
         paddingLeft: '20px',
         paddingRight: '20px',
-
+        
         '@media (min-width:768px)': {
+          maxWidth: '1400px',
           paddingLeft: '50px ',
           paddingRight: '50px',
-        }
-      }
-    }
-  }
-}
+        },
+      },
+    },
+  },
+};
