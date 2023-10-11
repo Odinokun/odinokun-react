c7cdb63f827884ebc36698a99d63b0f168cea8c2
odinokun <odinokun@gmail.com>
2023-10-11 15:06:16 +0200

    feat: social media added

---
+++ src/common/static/social_media.ts
@@ -0,0 +1,31 @@
+import { v1 } from 'uuid';
+import { Email, GitHub, LinkedIn } from '@mui/icons-material';
+
+interface ISocialMedia {
+  id: string;
+  label: string;
+  link: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  icon: any;
+}
+
+export const SOCIAL_MEDIA: ISocialMedia[] = [
+  {
+    id: v1(),
+    label: 'E-mail',
+    link: 'mailto:odinokun@gmail.com',
+    icon: Email,
+  },
+  {
+    id: v1(),
+    label: 'GitHub',
+    link: 'https://github.com/Odinokun',
+    icon: GitHub,
+  },
+  {
+    id: v1(),
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/dmitry-gordienko/',
+    icon: LinkedIn,
+  },
+];
