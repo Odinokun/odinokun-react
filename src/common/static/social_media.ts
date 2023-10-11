77daec67297d9f79c89974d4bc6f1140a46686cd
odinokun <odinokun@gmail.com>
2023-10-11 15:20:40 +0200

    feat: social media added

--- src/common/static/social_media.ts
+++ src/common/static/social_media.ts
@@ -7,6 +7,8 @@ interface ISocialMedia {
   link: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   icon: any;
+  target?: string;
+  rel?: string;
 }
 
 export const SOCIAL_MEDIA: ISocialMedia[] = [
@@ -21,11 +23,15 @@ export const SOCIAL_MEDIA: ISocialMedia[] = [
     label: 'GitHub',
     link: 'https://github.com/Odinokun',
     icon: GitHub,
+    target: '_blank',
+    rel: 'noreferrer noopener nofollow',
   },
   {
     id: v1(),
     label: 'LinkedIn',
     link: 'https://www.linkedin.com/in/dmitry-gordienko/',
     icon: LinkedIn,
+    target: '_blank',
+    rel: 'noreferrer noopener nofollow',
   },
 ];
